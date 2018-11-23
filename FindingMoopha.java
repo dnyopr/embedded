ecd592edb85260a85586f5fb513d084e27ff8016
dnyopr <58070042@kmitl.ac.th>
2018-11-23 13:31:44 +0700

    Add files via upload
    Final Finding Moopha

---
+++ FindingMoopha.java
@@ -0,0 +1,105 @@
+package myproject;
+import lejos.nxt.*;
+
+public class FindingMoopha {
+
+	/**
+	 * @param args
+	 */
+	
+	private static int soundThreshold = 70;
+    private static SoundSensor sound = new SoundSensor(SensorPort.S2);
+	
+    private static  void waitForLoudSound() throws Exception
+    {
+        int soundLevel;
+
+        Thread.sleep(500);
+        do
+        {
+            soundLevel = sound.readValue();
+        }
+        while ( soundLevel < soundThreshold );
+    }
+	
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+		int turnChk = 0;
+		TouchSensor touch = new TouchSensor(SensorPort.S1);
+		LightSensor light = new LightSensor(SensorPort.S3);
+		UltrasonicSensor UltraSonic = new UltrasonicSensor(SensorPort.S4);
+		
+		Motor.A.setSpeed(300);
+		Motor.B.setSpeed(300);
+		
+		waitForLoudSound();
+		
+		while(true){
+			UltraSonic = new UltrasonicSensor(SensorPort.S4);
+			
+			LCD.drawString("START", 0, 0);
+			//LCD.drawString("turnChk", 0, 1);
+			//LCD.drawInt(turnChk, 9, 1);
+			if(UltraSonic.getDistance() < 30){
+				if(turnChk < 4){
+					Motor.B.stop();
+					Motor.A.rotate(320);
+				}else{
+					break;
+				}
+				turnChk++;
+			}else{
+				Motor.A.forward();
+				Motor.B.forward();
+			}
+		}
+		Motor.A.stop();
+		Motor.B.stop();
+		LCD.drawString("FIRST POINT", 0, 0);
+		Thread.sleep(3000);
+		Motor.B.rotate(360);
+		
+		while(!touch.isPressed()){
+			Motor.A.forward();
+			Motor.B.forward();
+		}
+		Motor.A.stop();
+		Motor.B.stop();
+		LCD.drawString("NOT FOUND    ", 0, 0);
+		Thread.sleep(3000);
+		
+		while(light.getNormalizedLightValue()>400){
+			Motor.A.backward();
+			Motor.B.backward();
+		}
+		Motor.B.stop();
+		Motor.A.rotate(380);
+		LCD.drawString("GO ON          ", 0, 0);
+		Thread.sleep(3000);
+		turnChk = 0;
+		while(true){
+			UltraSonic = new UltrasonicSensor(SensorPort.S4);
+			LCD.drawString("GO ON          ", 0, 0);
+			
+			if(UltraSonic.getDistance()<30){
+				if(turnChk == 0){
+					Motor.A.stop();
+					Motor.B.rotate(360);
+					turnChk = 1;
+				}
+				else{
+					Motor.A.stop();
+					Motor.B.stop();
+					break;
+				}
+			}else{
+				Motor.A.forward();
+				Motor.B.forward();
+			}	
+		}
+		LCD.drawString("FOUND               ", 0, 0);
+		Thread.sleep(5000);
+		System.exit(0);
+	}
+}
